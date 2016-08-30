0a3d3d43ca1213a853232bc641629b82959ca8fc
Chris Dare <chris.dare@just-eat.com>
2016-08-30 20:50:42 +0100

    Updated home view and removed OWIN for now

--- dot-not/dot-not/Controllers/HomeController.cs
+++ dot-not/dot-not/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dot_not.Models;
 
 namespace dot_not.Controllers
 {
     public class HomeController : Controller
     {
+        private IDotNotDBContext idb = new DotNotDBContext();
+        HomeViewModel model = new HomeViewModel();
+
+        public HomeController() { }
+
+        public HomeController(IDotNotDBContext context)
+        {
+            idb = context;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            model.Challenges = idb.Challenges.ToList();
+            return View(model);
         }
 
     }
---
+++ dot-not/dot-not/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace dot_not.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<ChallengeModel> Challenges { get; set; }
+    }
+}

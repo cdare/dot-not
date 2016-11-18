b9918bf8d586706c0d8d078cac715c7355edb539
cdare <chrisdare04@gmail.com>
2016-11-18 14:48:22 +0000

    added migrations

---
+++ dot-not/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace dot_not.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<dot_not.Models.DotNotDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "dot_not.Models.DotNotDBContext";
+        }
+
+        protected override void Seed(dot_not.Models.DotNotDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}

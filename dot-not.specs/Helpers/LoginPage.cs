16449c70dfcc88e3839ad42994d2dad23c37f73f
cdare <chrisdare04@gmail.com>
2016-11-21 15:46:08 +0000

    tidy tests

---
+++ dot-not.specs/Helpers/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace dot_not.specs.Helpers
+{
+    class LoginPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = "http://localhost/DotNot/Account/Login";
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.driver = webDriver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "page-title")]
+        public IWebElement PageTitle { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Email")]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.Id, Using = "LogIn")]
+        public IWebElement LogIn { get; set; }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+    }
+}
---
+++ dot-not.specs/Helpers/RegisterPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace dot_not.specs.Helpers
+{
+    class RegisterPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = "http://localhost/DotNot/Account/Register";
+
+        public RegisterPage(IWebDriver webDriver)
+        {
+            this.driver = webDriver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "page-title")]
+        public IWebElement PageTitle { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Email")]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.Id, Using = "ConfirmPassword")]
+        public IWebElement ConfirmPassword { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Register")]
+        public IWebElement Register { get; set; }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+    }
+}
